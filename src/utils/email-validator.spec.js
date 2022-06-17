2d12b0fe1c9747115187a42f6cbb9a8f0ac3c48e
Lucas Cardoso <lucascardoso946@gmail.com>
2022-06-17 14:18:07 -0300

    fix email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -8,20 +8,20 @@ const makeSut = () => {
 describe('Email Validator', () => {
   test('Should return true if validator returns true', () => {
     const sut = makeSut()
-    const isEmailValid = sut.isValid('valid_email@gmail.com')
+    const isEmailValid = sut.isValid('valid_email@email.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('Should return false if validator returns false', () => {
     validator.isEmailValid = false
     const sut = makeSut()
-    const isEmailValid = sut.isValid('invalid_email@gmail.com')
+    const isEmailValid = sut.isValid('invalid_email@email.com')
     expect(isEmailValid).toBe(false)
   })
 
   test('Should call validator with correct email', () => {
     const sut = makeSut()
-    sut.isValid('any_email@gmail.com')
-    expect(validator.email).toBe('any_email@gmail.com')
+    sut.isValid('any_email@email.com')
+    expect(validator.email).toBe('any_email@email.com')
   })
 })
