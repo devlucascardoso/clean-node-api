79e02895229224eae45208989dbdc2996c0f0489
Lucas Cardoso <lucascardoso946@gmail.com>
2022-06-22 15:26:26 -0300

    Should call JWT with correct values

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -2,16 +2,20 @@ const jwt = require('jsonwebtoken')
 const MissingParamError = require('../errors/missing-param-error')
 
 class TokenGenerator {
+  constructor (secret) {
+    this.secret = secret
+  }
+
   async generate (id) {
     if (!id) {
       throw new MissingParamError('id')
     }
-    return jwt.sign(id, 'secret')
+    return jwt.sign(id, this.secret)
   }
 }
 
 const makeSut = () => {
-  return new TokenGenerator()
+  return new TokenGenerator('secret')
 }
 
 describe('Token Generator', () => {
@@ -26,5 +30,12 @@ describe('Token Generator', () => {
     const sut = makeSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
+  })
+
+  test('Should call JWT with correct values', async () => {
+    const sut = makeSut()
+    await sut.generate('any_id')
+    expect(jwt.id).toBe('any_id')
+    expect(jwt.secret).toBe(sut.secret)
   })
 })
